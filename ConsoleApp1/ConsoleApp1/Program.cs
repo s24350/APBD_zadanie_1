5c896110671967b72471ec54a924b0efc2ba4b55
unknown <s24350@pjwstk.edu.pl>
2024-03-17 11:47:52 +0100

    count-average

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,6 +2,8 @@
 Console.WriteLine("Hello, World!");
 x.B.SaySomething();
 
+var average = x.B.countAverage(new int[] { 1, 2, 3, 4, 5, 6});
+Console.WriteLine(average);
 namespace x
 {
     class B
@@ -9,6 +11,15 @@ namespace x
         public static void SaySomething()
         {
             Console.WriteLine("Hello Hello APBD");
+        }
+
+        public static double countAverage(int[] x) { 
+            double sum = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                sum+= x[i];
+            }
+            return sum / x.Length;
         }
     }
 }
